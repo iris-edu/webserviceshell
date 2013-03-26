c184cf3cf5e4a34bb179d0400c7f0525b7fa7ddf
Rich Karstens <rich@iris.washington.edu>
2013-03-26 18:53:49 +0000

    Added Copyright notice

--- src/edu/iris/wss/utils/WebUtils.java
+++ src/edu/iris/wss/utils/WebUtils.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IRIS DMC supported by the National Science Foundation.
+ *  
+ * This file is part of the Web Service Shell (WSS).
+ *  
+ * The WSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * The WSS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * A copy of the GNU Lesser General Public License is available at
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package edu.iris.wss.utils;
 
 import java.io.StringReader;
