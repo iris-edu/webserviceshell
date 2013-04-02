5072f545d091132bffe73f3d8af605759e6c3128
Rich Karstens <rich@iris.washington.edu>
2013-04-02 16:31:08 +0000

    1.0.1

--- src/edu/iris/wss/framework/AppConfigurator.java
+++ src/edu/iris/wss/framework/AppConfigurator.java
@@ -32,7 +32,7 @@ import org.apache.log4j.Logger;
 
 public class AppConfigurator {
 
-	public static final String wssVersion = "1.0.0";
+	public static final String wssVersion = "1.0.1";
 	public static final String wssConfigDirSignature = "wssConfigDir";
 
 	public static final String wssDigestRealmnameSignature = "wss.digest.realmname";
