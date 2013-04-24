1759196bdc3edd21f5c7ebec44d7a21b03e9871d
Rich Karstens <rich@iris.washington.edu>
2013-04-24 21:09:16 +0000

    format vs. output rehash for query parameters

--- src/edu/iris/wss/framework/AppConfigurator.java
+++ src/edu/iris/wss/framework/AppConfigurator.java
@@ -32,7 +32,7 @@ import org.apache.log4j.Logger;
 
 public class AppConfigurator {
 
-	public static final String wssVersion = "1.0.3";
+	public static final String wssVersion = "1.0.4";
 	public static final String wssConfigDirSignature = "wssConfigDir";
 
 	public static final String wssDigestRealmnameSignature = "wss.digest.realmname";
