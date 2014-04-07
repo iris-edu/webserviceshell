d01b9ba423d10a3016ca0594359139787e779f0c
Yazan Suleiman <yazan@iris.washington.edu>
2014-04-07 15:56:49 +0000

    Mavenized correctly with all dependencies, Fixed start, end date logging

---
+++ src/main/java/edu/iris/wss/IrisStreamingOutput/RecordMetaData.java
@@ -0,0 +1,58 @@
+package edu.iris.wss.IrisStreamingOutput;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class RecordMetaData {
+
+	private Long size;
+	private Date start;
+	private Date end;
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+	public Long getSize() {
+		return size;
+	}
+
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public void setStart(String start) throws ParseException {
+		Date d = sdf.parse(start);
+		if (this.start != null) {
+			if (d.after(this.start)) {
+				return;
+			}
+		}
+		this.start = d;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public void setEnd(String end) throws ParseException {
+		Date d = sdf.parse(end);
+		if (this.end != null) {
+			if (d.before(this.end)) {
+				return;
+			}
+		}
+		this.end = d;
+	}
+}
