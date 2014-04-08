7c64ad1ec6575e20eb68a7fa1383f737d8afb65f
Yazan Suleiman <yazan@iris.washington.edu>
2014-04-08 21:09:23 +0000

    Has the time parse correctly for logging

--- src/main/java/edu/iris/wss/IrisStreamingOutput/RecordMetaData.java
+++ src/main/java/edu/iris/wss/IrisStreamingOutput/RecordMetaData.java
@@ -1,20 +1,18 @@
 package edu.iris.wss.IrisStreamingOutput;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.joda.time.LocalDateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
+
+import edu.sc.seis.seisFile.mseed.Btime;
 
 public class RecordMetaData {
 
 	private Long size;
 	private Date start;
 	private Date end;
-    
-    // replaced with Joda data parsing because simple java parasing was adding
-    // time related something like SSSS/1000 
-    DateTimeFormatter jodaFmt = DateTimeFormat.forPattern("yyyy,DDD,HH:mm:ss.SSSS");
+
+	SimpleDateFormat fmt = new SimpleDateFormat("yyyy,DDD,HH:mm:ss.SSSS");
 
 	public Long getSize() {
 		return size;
@@ -32,18 +30,17 @@ public class RecordMetaData {
 		this.start = start;
 	}
 
-    public void setIfEarlier(String start) throws ParseException {
-        LocalDateTime jodaDate = LocalDateTime.parse(start, jodaFmt);
-        Date d = jodaDate.toDate();
-
-        if (this.start != null) {
-            if (d.before(this.start)) {
-                this.start = d;
-            }
-        } else {
-            this.start = d;
-        }
-    }
+	public void setIfEarlier(String start) throws ParseException {
+		start = start.replaceAll("(\\.[0-9]{3})[0-9]*( [AP]M)", "$1$2");
+		Date d = fmt.parse(start);
+		if (this.start != null) {
+			if (d.before(this.start)) {
+				this.start = d;
+			}
+		} else {
+			this.start = d;
+		}
+	}
 
 	public Date getEnd() {
 		return end;
@@ -53,16 +50,16 @@ public class RecordMetaData {
 		this.end = end;
 	}
 
-    public void setIfLater(String end) throws ParseException {
-        LocalDateTime jodaDate = LocalDateTime.parse(end, jodaFmt);
-        Date d = jodaDate.toDate();
-        
-        if (this.end != null) {
-            if (d.after(this.end)) {
-            this.end = d;
-            }
-        } else {
-            this.end = d;
-        }
-    }
+	public void setIfLater(String end) throws ParseException {
+		end = end.replaceAll("(\\.[0-9]{3})[0-9]*( [AP]M)", "$1$2");
+		Date d = fmt.parse(end);
+
+		if (this.end != null) {
+			if (d.after(this.end)) {
+				this.end = d;
+			}
+		} else {
+			this.end = d;
+		}
+	}
 }
